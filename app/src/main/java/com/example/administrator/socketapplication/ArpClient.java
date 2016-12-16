0ee51d1e4dfd05cad9c45d00061d2e17c4cf790f
Pi Zipeng <lxpzp123@gmail.com>
2016-12-16 19:42:18 +0800

    add ArpClient.java for send arp packet by socket

---
+++ app/src/main/java/com/example/administrator/socketapplication/ArpClient.java
@@ -0,0 +1,14 @@
+package com.example.administrator.socketapplication;
+
+import java.net.Socket;
+
+/**
+ * Created by Administrator on 2016/12/16.
+ * ArpClient is a Client to send arp packet by socket.
+ */
+
+public class ArpClient {
+    private Socket socket;
+
+    private void ArpClient() {};
+}
